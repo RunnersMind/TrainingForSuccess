b3fbd11acc6a8601cfca4cb8ce17de16f0023045
Pat Carlson <webdevpatricia@gmail.com>
2018-04-19 14:32:46 -0700

    fixed searchcomponent and results

--- client/src/components/Search/SearchComponent.js
+++ client/src/components/Search/SearchComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SearchButton from "./button.js";
 import SearchInput from "./input.js";
 import SearchMap from "./map.js";
-import SearchResults from "./results.js";
+// import SearchResults from "./results.js";
 import "./search.css";
 // import API from "../../utils/API";
 import { Form, FormGroup, Col } from "reactstrap";
@@ -19,9 +19,9 @@ class SearchComponent extends Component{
                     <h1 className="display-5 text-center mt-5">Find training in your area...</h1>      
                     <SearchMap onStateClick={this.onStateClick} />
                     <SearchInput />
-                <SearchResults>
+                {/* <SearchResults>
                     <SearchButton />
-                </SearchResults> 
+                </SearchResults>  */}
             </div>   
         );
     }
